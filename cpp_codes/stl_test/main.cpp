993659e7fa655c1ee7a3aef8c186beafc0d0c46f
12e21 <2496813401@qq.com>
2022-12-14 00:05:22 +0800

    添加了map函数笔记

--- cpp_codes/stl_test/main.cpp
+++ cpp_codes/stl_test/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <map>
 #include <utility>
 using namespace std;
 int main() {
-    pair<string,string> pair1("google","www.google.com");
-    pair<string,string> pair2("baidu","www.baidu.com");
-    pair2.swap(pair1);
-    cout<<pair2.first<<":"<<pair2.second;
-
+    map<string,string> map1{{"1","一"},{"2","二"}};
+    map1.insert(make_pair("3","三"));
+    map1.emplace(make_pair("4","四"));
+    map1.erase("2");
+    for(auto it=map1.cbegin();it!=map1.cend();it++)
+    {
+        cout<<it->first<<":"<<it->second<<endl;
+    }
 }
